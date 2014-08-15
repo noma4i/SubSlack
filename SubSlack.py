c82684b5c41d2ff98e715b08bb55fdccff89f95f
noma4i <noma4i@gmail.com>
2014-08-15 19:03:33 +1000

    working implementation

--- SubSlack.py
+++ SubSlack.py
@@ -1,9 +1,24 @@
 import sublime
 import sublime_plugin
+import sys
+import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
+
+from pyslack import SlackClient
+
+class Slackchat:
+  def __init__(self):
+    settings = sublime.load_settings("SubSlack.sublime-settings")
+    token  = settings.get('token')
+    self.room  = '#%s' % settings.get('default_chat')
+    self.client = SlackClient(token)
+
+  def post(self,text):
+    self.client.chat_post_message(self.room, "testing, testing...",username='alex')
 
 
 class SubSlackCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    print('Cool!')
+    chat = Slackchat()
+    chat.post("dddd")
